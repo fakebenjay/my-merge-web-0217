74751f694f9b771a1936d7aeb5d911c63963ae1b
Katie Hoffman <ktahoffman@gmail.com>
2015-04-02 09:37:53 -0400

    add solution

--- lib/my_merge.rb
+++ lib/my_merge.rb
@@ -1,3 +1,13 @@
-# add the method #my_merge to the Hash class
-
-# it should accept an argument and an optional block
+class Hash 
+  def my_merge(other_hash, &block)
+    keys = (self.keys + other_hash.keys)
+    keys.each_with_object(Hash.new) do |key, merge|
+      next if merge.has_key? key
+      if keys.count(key) > 1 && block_given?
+        merge[key] = block.call(key, self[key], other_hash[key])
+      else
+        merge[key] = other_hash[key] || self[key]
+      end
+    end
+  end
+end
